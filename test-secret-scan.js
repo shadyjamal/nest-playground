a0e319095a946a73585a2ef67dd17e1db75bc491
chadi jamal <47235289+shadyjamal@users.noreply.github.com>
2025-09-24 16:24:18 +0200

    Create test-secret-scan.js

---
+++ test-secret-scan.js
@@ -0,0 +1,12 @@
+// Example.js
+
+const apiKey = "12345-ABCDE-SECRET-API-KEY";
+const dbPassword = "SuperSecr3tPassw0rd!";
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...";
+
+function connectToDatabase() {
+  console.log("Connecting with password:", dbPassword);
+}
+
+function callExternalApi() {
+  console.log("Using API key
